33fabd20e2fdc192bc6827da23ed7bebfcd21c19
‪Taha Al-rehany‬‏ <32350359+taharehany@users.noreply.github.com>
2022-08-27 12:29:30 +0200

    first

--- resources/views/front/portfolio/show.blade.php
+++ resources/views/front/portfolio/show.blade.php
@@ -5,27 +5,32 @@
 @section('content')
 <!--single portfolio-->
 <section class="single-portfolio">
-    <div class="container">
-        <div class="main-title">
-            <h2>Lince tech branding</h2>
-            <p>ABOUT : When it comes to the quality of the fence system, you are in the right place with Lince UG. We are a company that specializes in individual solutions in the field of slat and industrial fences Our most requested products are high-quality design fences made of lamellar aluminium.</p>
-            <p>Mission : Our mission was to rebuild the brand to suit the modern time and also to suit the company's modern trends and its new distinguished services.</p>
-        </div>
-        <div class="content">
-            <div class="row">
-               @if(count($project->Details))
-               @foreach ($project->Details as $img)
-               <!-- <div class="{{(is_mobile()) ? 'col-'.$img->mobile_col:'col-md-'.$img->desktop_col }} box"> -->
-               <div class="col-{{$img->mobile_col}} col-md-{{$img->desktop_col}} box">
-                  <a class="box-image" data-src="{{ asset($img->image) }}" href="" data-fancybox="demo">
-                     <img class="img-fluid" src="{{ asset($img->image) }}" alt="alt">
-                  </a>
-               </div>
-               @endforeach
-               @endif
+   <div class="container">
+      <div class="main-image">
+         <a class="box-image" data-src="{{ asset($project->main_image) }}" href="" data-fancybox="demo">
+            <img class="img-fluid" src="{{ asset($project->main_image) }}" alt="alt">
+         </a>
+      </div>
+      <div class="main-title">
+         <h2>Lince tech branding</h2>
+         <p>ABOUT : When it comes to the quality of the fence system, you are in the right place with Lince UG. We are a company that specializes in individual solutions in the field of slat and industrial fences Our most requested products are high-quality design fences made of lamellar aluminium.</p>
+         <p>Mission : Our mission was to rebuild the brand to suit the modern time and also to suit the company's modern trends and its new distinguished services.</p>
+      </div>
+      <div class="content">
+         <div class="row">
+            @if(count($project->Details))
+            @foreach ($project->Details as $img)
+            <!-- <div class="{{(is_mobile()) ? 'col-'.$img->mobile_col:'col-md-'.$img->desktop_col }} box"> -->
+            <div class="col-{{$img->mobile_col}} col-md-{{$img->desktop_col}} box">
+               <a class="box-image" data-src="{{ asset($img->image) }}" href="" data-fancybox="demo">
+                  <img class="img-fluid" src="{{ asset($img->image) }}" alt="alt">
+               </a>
             </div>
-        </div>
-    </div>
+            @endforeach
+            @endif
+         </div>
+      </div>
+   </div>
 </section>
 <!--single portfolio-->
 @endsection
