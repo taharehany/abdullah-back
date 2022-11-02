d60220de461d055ed29f1aec2a6380365b369f21
‪Taha Al-rehany‬‏ <32350359+taharehany@users.noreply.github.com>
2022-11-02 09:19:00 +0200

    first

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -1,5 +1,8 @@
-@include('front.layouts.header')
+{{-- @include('front.layouts.header')
 
     @yield('content')
 
-@include('front.layouts.footer')
+@include('front.layouts.footer') --}}
+
+
+<img class="text img-fluid" src="{{ asset('front/images/coming.jpg') }}" alt="alt" style="width: 100%; height: 97vh; object-fit: contain">
